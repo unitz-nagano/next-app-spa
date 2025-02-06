fe1dc794935c2e60ef3a82b0b27663935496a79f
Suzu Nagano <nagano@unitz.jp>
2025-02-06 19:34:01 +0900

    Fix:buildに失敗するので修正

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,10 @@ const nextConfig: NextConfig = {
   output: "export",
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  trailingSlash: true,
+  // trailingSlash: true,
 
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  skipTrailingSlashRedirect: true,
+  // skipTrailingSlashRedirect: true,
 
   // Optional: Change the output directory `out` -> `dist`
   distDir: "dist",
--- src/app/bbb/ccc/page.tsx
+++ src/app/bbb/ccc/page.tsx
@@ -3,17 +3,29 @@
 import NextLink from "next/link"
 import style from "@/app/page.module.css"
 import { useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react"
+import { Suspense } from "react"
+
+function Search() {
+  const [id, setId] = useState<string | null>(null)
+
+  const searchParams = useSearchParams()
+  useEffect(() => {
+    setId(searchParams.get("id"))
+  }, [searchParams])
+  return <div>id:{id}</div>
+}
 
 export default function CCC() {
   // URLのパラメーターを取得
-  const searchParams = useSearchParams()
-  const id = searchParams.get("id")
 
   return (
     <div className={style.page}>
       <div className={style.main}>
         <h1>CCC</h1>
-        <div>id:{id}</div>
+        <Suspense>
+          <Search />
+        </Suspense>
         <ul>
           <li>
             <NextLink href="/" className="underline text-blue-600">
